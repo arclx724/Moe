dfd02ffffeb5061a4573d2b388a0a279d3b2c464
arclx724 <arclx724@gmail.com>
2026-04-25 00:03:12 +0530

    Update play.py

--- ShrutiMusic/plugins/play/play.py
+++ ShrutiMusic/plugins/play/play.py
@@ -361,7 +361,7 @@ async def play_commnd(
         streamtype = "youtube"
     if str(playmode) == "Direct":
         if not plist_type:
-            if details["duration_min"]:
+            if details.get("duration_min"):
                 duration_sec = time_to_seconds(details["duration_min"]) if details.get("duration_min") not in [None, "N/A"] else 0
                 if duration_sec > config.DURATION_LIMIT:
                     return await mystic.edit_text(
